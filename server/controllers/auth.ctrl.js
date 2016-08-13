2cfbb9c5a2d46fe0120cebdd087abc05b100599f
Michael Rico <rico.michael@gmail.com>
2016-08-12 19:37:50 -0700

    Begin register

--- server/controllers/auth.ctrl.js
+++ server/controllers/auth.ctrl.js
@@ -2,7 +2,15 @@ var db = require('../db');
 var User = db.User;
 
 var registerUser = function(req, res) {
-  res.send('still working');
+  var username = req.body.username;
+  var password = req.body.password;
+  User.findOne({ where: { username: username } })
+    .then(function(user) {
+      if (user) {
+        return res.send(409);
+      }
+      User.create({username: username, password: password});
+    });
 };
 
 module.exports = {
